4ed9735a5e5696f94dfff46319e480c41987d5e1
Demi Adisa <demiadisa@gmail.com>
2022-02-25 23:39:26 -0400

    Landing Page Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spaced_repetition_app/screens/landing_screen.dart';
 import 'package:spaced_repetition_app/screens/splash_screen.dart';
 
 void main() {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
       ),
-      home: const SplashScreen(),
+      home: const LandingScreen(),
     );
   }
 }
---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget
+{
+
+  const LandingScreen();
+
+  @override
+  State<LandingScreen> createState() => _LandingScreen();
+
+
+}
+
+class _LandingScreen extends State<LandingScreen>
+{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            color: const Color(0xFFDC1A22),
+            child: Center(
+                child: Column(
+                  children: <Widget>[
+                    const Image(
+                      image: AssetImage("assets/logo.jpeg"),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Lets Get you Started!",
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20.0
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                      const Text(
+                      "Are you a New Member?",
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 12.0
+                        ),
+                      ),
+                          ElevatedButton(
+                            onPressed: () { print("I was clicked");},
+                            child: Text("Register"),
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(100, 35)
+                            )
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                    ),
+
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          const Text(
+                            "Already have an account?",
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 12.0
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () { print("I was clicked");},
+                            child: Text("Sign In"),
+                            style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(100, 35)
+                            )
+
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                    )
+                  ],
+
+                )
+            )));
+  }
+
+}
