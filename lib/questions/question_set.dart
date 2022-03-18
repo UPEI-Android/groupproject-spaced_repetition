21c25204b1c1829ba4605b9dfb43a376dd07fb87
Demi Adisa <demiadisa@gmail.com>
2022-03-18 09:57:56 -0300

    Added a few new files for the Question folder

---
+++ lib/questions/question_set.dart
@@ -0,0 +1,15 @@
+import 'package:spaced_repetition_app/questions/question_class.dart';
+
+///This will serve an object for a set of questions
+///It will group alike questions for the users ease
+///Maybe the questions are related by Subject or a particular field
+///This class can help achieve that
+class QuestionSet
+{
+  final String setName;
+  final List<Question> questionList;
+
+  QuestionSet( {required this.setName, required this.questionList});
+
+  int get setSize => questionList.length;
+}
