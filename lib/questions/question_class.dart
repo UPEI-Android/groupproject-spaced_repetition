e1c82bac8a0d1487d136bc50845222a3a9d5dd48
Demi Adisa <demiadisa@gmail.com>
2022-03-29 12:05:10 -0300

    Question Class Implemented

--- lib/questions/question_class.dart
+++ lib/questions/question_class.dart
@@ -1,5 +1,39 @@
-import 'package:flutter/material.dart';
+///This is the question class.
+///This class contains the question, the answer and the chosen date that the question will pop up.
+///The date will be chosen by divide the input of the time interval the user want
+///by 10-20-40-70-100% of the interval based on the spaced repetition method.
 
-class Question{
+class Question {
+  //question related
+  String questionText;
+  String answerText;
 
+  //Course
+  String courseName;
+
+  //Spaced Repetition related
+  int step;
+  int duration;
+  DateTime nextReview;
+
+  //constructor for question class
+  Question(
+      {required this.questionText,
+        required this.answerText,
+        required this.courseName,
+        required this.step,
+        required this.duration,
+        required this.nextReview});
+
+  //get the question and answer
+  String get returnQuestionText => this.questionText;
+
+  String get returnQuestionAns => this.answerText;
+
+  //Retrieve spaced repetition algo values
+  int get returnStep => this.step;
+
+  int get returnDuration => this.duration;
+
+  DateTime get returnNextReview => this.nextReview;
 }
